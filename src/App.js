e55592cfcfdec0759b228d6b525283aea4b81671
emillyalmeida <emillyalmeidan@gmail.com>
2022-03-31 23:09:53 -0300

    update: add components principais in the app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,26 @@ function App() {
   return (
     <div className="App">
       {login ? (
-        <header className="App-header"></header>
+        <>
+          <header className="App-header">
+            <h1>
+              <img src="../public/img/Nu_Kenzie.svg" alt="Nu kenzie" />
+            </h1>
+            <button onClick={() => setlogin(false)}>Inicio</button>
+          </header>
+          <section className="container-total">
+            <section className="container-form"></section>
+            <section className="conatiner-list">
+              <div className="filtros">
+                <h3>Resumo finaceiro</h3>
+                <button>Todos</button>
+                <button>Entradas</button>
+                <button>Despesas</button>
+              </div>
+              <div className="financeiro"></div>
+            </section>
+          </section>
+        </>
       ) : (
         <section className="Home">
           <section className="entrar">
